083cc685f9b76580c811c7559f8abd47738678e9
Adam Doan <adam.c.doan@gmail.com>
2017-03-14 10:13:02 -0400

    Now with CSVWriter class

---
+++ CSVWriter/CSVDictWriter.py
@@ -0,0 +1,20 @@
+import csv
+import os.path
+from . import CSVWriter
+
+class CSVDictWriter(CSVWriter):
+
+    def write(self, data):
+        new_file = False
+
+        if not os.path.isfile(self.out_file_path):
+            new_file = True
+
+        with open(self.out_file_path, 'wb') as f:
+            fields = data.keys()
+            writer = csv.DictWriter(f, fieldnames=fields)
+
+            if new_file:
+                writer.writeheader()
+
+            writer.writerows(data)
---
+++ CSVWriter/CSVWriter.py
@@ -0,0 +1,7 @@
+class CSVWriter():
+
+    def __init__(self, out_file_path):
+        self.out_file_path = out_file_path
+
+    def write(self, data):
+       raise NotImplementedError
