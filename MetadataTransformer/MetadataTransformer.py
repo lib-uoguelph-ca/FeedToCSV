3a31c388a63f40d589660c34d83c48010874dacd
Adam Doan <adam.c.doan@gmail.com>
2017-03-20 16:19:39 -0400

    Now with basics of metadata transformer.

--- MetadataTransformer/MetadataTransformer.py
+++ MetadataTransformer/MetadataTransformer.py
@@ -1,8 +1,22 @@
+from typing import Iterable, Any, Mapping
+
 class MetadataTransformer:
     def __init__(self):
-        pass
+        self.mapping = {}
 
-    def transform(self, data):
-        raise NotImplementedError
+    def transform(self, data: Iterable[ Mapping[str, Any] ]):
+        for row in data:
+            self._transform_row(row)
 
-    
+    def _transform_row(self, row):
+
+        result = {}
+        for key in self.mapping():
+            if callable(self.mapping[key]):
+                result[key] = self.mapping[key](row)
+            elif isinstance(self.mapping[key], str):
+                result[key] = row[self.mapping[key]]
+            else:
+                raise KeyError("Metadata Transformer: Invalid mapping")
+
+        return result
