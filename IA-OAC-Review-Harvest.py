2ec9c5acd3893ff1d34e211b723d5fc74b03ce51
Adam Doan <adam.c.doan@gmail.com>
2018-06-25 09:07:16 -0400

    Now with script to harvest OAC review collection.

---
+++ IA-OAC-Review-Harvest.py
@@ -0,0 +1,17 @@
+from consumer.ia_consumer import IAConsumer
+from writer.csv_dict_writer import CSVDictWriter
+from transformer.ia_metadata_transformer import IAMetadataTransformer
+import logging
+
+# Set up logging
+logger = logging.getLogger('FeedToCSV')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(logging.StreamHandler())
+fh = logging.FileHandler('output/errors.log')
+fh.setLevel(logging.ERROR)
+logger.addHandler(fh)
+
+writer = CSVDictWriter('output/harvest.csv')
+transformer = IAMetadataTransformer()
+consumer = IAConsumer(transformer=transformer, writer=writer, logger=logger, collection='oac_review')
+consumer.process()
