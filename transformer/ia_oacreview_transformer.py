69140f61a1964f87e5c259bec67865ea8c5c009d
Adam Doan <adam.c.doan@gmail.com>
2018-06-27 14:26:28 -0400

    Now with updated metadata mapping from OAC Review team.

--- transformer/ia_oacreview_transformer.py
+++ transformer/ia_oacreview_transformer.py
@@ -4,24 +4,32 @@ class IAOACReviewTransformer(MetadataTransformer):
     def __init__(self):
         super().__init__()
         self.mapping = {
-            'dc.title en': 'title',
-            'dc.contributor': 'contributor',
-            'dc.contributor.affiliation': self._get_uofg,
-            'dc.contributor.editor': self._get_empty,
+            'dc.title': 'title',
+            'dc.creator': 'creator',
+            'dc.contributor': self._get_contributor,
+            'dc.contributor.affiliation': self._get_contributor_affiliation,
+            'dc.contributor.editor': 'editor',
             'dc.date.issued': 'date',
-            'dc.date.copyright': 'date',
+            'dc.date.copyright': 'year',
             'dc.description.abstract': 'description',
             'dc.description.tableofcontents': 'description_tableofcontents',
             'dc.language.iso': 'language',
             'dc.publisher': 'publisher',
             'dc.relation.ispartofseries': 'series',
-            'dc.rights.holder': self._get_uofg,
+            'dc.rights': self._get_empty,
+            'dc.rights.holder': 'rights',
             'dc.rights.uri': 'licensurl',
-            'dc.source': 'identifier-access',
             'dc.subject': self._get_subjects,
             'dc.type': 'type',
             'files': self._get_files,
         }
+    def _get_contributor(self, row):
+        values = row['contributor'].split(',')
+        return values[0]
+
+    def _get_contributor_affiliation(self, row):
+        values = row['contributor'].split(',')
+        return values[1]
 
     def _get_uofg(self, row):
         return "University of Guelph"
